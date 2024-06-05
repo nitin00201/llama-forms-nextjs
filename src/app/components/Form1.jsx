77966b6b38e7ec68b05b95b14e6d3b5da033c445
nitin002012 <nitin.kabi@hyscaler.com>
2024-06-05 10:45:25 +0530

    navbar

---
+++ src/app/components/Form1.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+const Form1=()=>{
+  return(
+    <>
+    
+    </>
+  )
+}
+return Form1
---
+++ src/app/demo/Page.jsx
@@ -0,0 +1,27 @@
+
+export var dummyData = [
+        {
+          question: "i am question 1",
+          answer: "i am answer 1",
+        },
+        {
+          question: "i am question 2",
+          answer: "i am answer 2",
+        },
+        {
+          question: "i am question 3",
+          answer: "i am answer 3",
+        },
+      ];
+const Page2 =()=>{
+   
+    
+    return ( 
+    <>
+
+    </>
+    );
+}
+
+export default Page2
+
